20686903ee12b918dfd7d4ff4db2631a9060968b
Techfrolic <developer@techfrolic.com>
2024-08-04 15:00:23 +0530

    build

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,8 +2,8 @@
  *
  */
 export const ALERT_INFO = "Info";
-// export const APIHEADER = "13.55.239.220";
-export const APIHEADER = "localhost";
+export const APIHEADER = "13.55.239.220";
+// export const APIHEADER = "localhost";
 export const ALERT_WARNING = "Warning";
 export const ALERT_ERROR = "Error";
 export const ALERT_SUCCESS = "Success";
